2a80aa41746b56b1fb820cc927dd224d6b25407a
Kyle Unruh <kunruh9@gmail.com>
2025-04-27 11:09:50 -0700

    Update base path in Vite configuration from '/vocab-game/' to '/vocab_game/' for consistency in deployment

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,6 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/vocab-game/',
+  base: '/vocab_game/',
   plugins: [react(), tailwindcss()],
 })
